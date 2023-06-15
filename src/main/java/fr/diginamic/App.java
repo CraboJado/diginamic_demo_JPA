174774c595b0492f38099b496c15049ce27ec608
liting <liting_qiu@hotmail.com>
2023-06-15 14:22:23 +0200

    set up slf4j logger

--- src/main/java/fr/diginamic/App.java
+++ src/main/java/fr/diginamic/App.java
@@ -3,12 +3,16 @@ package fr.diginamic;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class App {
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
     public static void main(String[] args) {
         try(EntityManagerFactory emFactory = Persistence.createEntityManagerFactory("demo_JPA_cleverCloud");
             EntityManager em =emFactory.createEntityManager();) {
             System.out.println(em);
+            LOG.info("connection is" + em);
         }
     }
 }
